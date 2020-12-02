65a618e51937552a0dcf85e493b3208baa45b77c
Sunjay Varma <varma.sunjay@gmail.com>
2020-12-01 21:10:15 -0800

    Brute force solving day 1a

--- src/day1.rs
+++ src/day1.rs
@@ -8,12 +8,24 @@ use crate::util;
 pub struct Day1Args {
     /// The input file to read
     input: PathBuf,
+    /// The target value that two expenses must sum to
+    #[structopt(default_value = "2020")]
+    target: i64,
 }
 
 pub fn day1(args: Day1Args) -> anyhow::Result<()> {
-    let Day1Args {input} = args;
+    let Day1Args {input, target} = args;
 
     let expenses: Vec<i64> = util::read_lines(input)?.collect::<anyhow::Result<Vec<_>>>()?;
+
+    for &expense1 in &expenses {
+        for &expense2 in &expenses {
+            if expense1 + expense2 == target {
+                println!("{} + {} = {}", expense1, expense2, expense1 + expense2);
+                println!("{} * {} = {}", expense1, expense2, expense1 * expense2);
+            }
+        }
+    }
 
     Ok(())
 }
