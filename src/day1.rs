72bb54f294cb78a4e140dac457abd2da0064580c
Sunjay Varma <varma.sunjay@gmail.com>
2020-12-01 21:40:36 -0800

    Implementing day1b (essentially three sum)

--- src/day1.rs
+++ src/day1.rs
@@ -35,3 +35,46 @@ pub fn day1a(args: Day1A) -> anyhow::Result<()> {
 
     Ok(())
 }
+
+#[derive(Debug, StructOpt)]
+pub struct Day1B {
+    /// The input file to read
+    #[structopt(default_value = "input/day1")]
+    input: PathBuf,
+    /// The target value that three expenses must sum to
+    #[structopt(default_value = "2020")]
+    target: i64,
+}
+
+/// This problem is essentially Three Sum
+pub fn day1b(args: Day1B) -> anyhow::Result<()> {
+    let Day1B {input, target} = args;
+
+    let expenses: Vec<i64> = util::read_lines(input)?.collect::<anyhow::Result<_>>()?;
+    for (i, expense) in expenses.iter().enumerate() {
+        let rest = &expenses[i+1..];
+        let remaining = target - expense;
+
+        if let Some((other1, other2)) = two_sum(rest, remaining) {
+            println!("{} + {} + {} = {}", expense, other1, other2, expense + other1 + other2);
+            println!("{} * {} * {} = {}", expense, other1, other2, expense * other1 * other2);
+        }
+    }
+
+    Ok(())
+}
+
+fn two_sum(numbers: &[i64], target: i64) -> Option<(i64, i64)> {
+    let mut seen = HashSet::new();
+
+    for &value in numbers {
+        // The other value that would be needed to add up to target
+        let other = target - value;
+        if seen.contains(&other) {
+            return Some((value, other));
+        }
+        seen.insert(value);
+    }
+
+    None
+}
--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use structopt::StructOpt;
 enum AdventOfCode {
     #[structopt(name = "day1a")]
     Day1A(day1::Day1A),
+    #[structopt(name = "day1b")]
+    Day1B(day1::Day1B),
 }
 
 fn main() -> anyhow::Result<()> {
@@ -16,5 +18,6 @@ fn main() -> anyhow::Result<()> {
     use AdventOfCode::*;
     match opts {
         Day1A(args) => day1::day1a(args),
+        Day1B(args) => day1::day1b(args),
     }
 }
