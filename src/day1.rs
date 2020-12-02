3427011f2902e3525e6b518eff26f4f18d448cda
Sunjay Varma <varma.sunjay@gmail.com>
2020-12-01 21:17:05 -0800

    Linear time solution based on Two Sum (also only loads one line into memory at a time) - O(n) runtime and memory

--- src/day1.rs
+++ src/day1.rs
@@ -1,4 +1,4 @@
-use std::path::PathBuf;
+use std::{collections::HashSet, path::PathBuf};
 
 use structopt::StructOpt;
 
@@ -7,24 +7,30 @@ use crate::util;
 #[derive(Debug, StructOpt)]
 pub struct Day1Args {
     /// The input file to read
+    #[structopt(default_value = "input/day1a")]
     input: PathBuf,
     /// The target value that two expenses must sum to
     #[structopt(default_value = "2020")]
     target: i64,
 }
 
+/// This problem is essentially Two Sum
 pub fn day1(args: Day1Args) -> anyhow::Result<()> {
     let Day1Args {input, target} = args;
 
-    let expenses: Vec<i64> = util::read_lines(input)?.collect::<anyhow::Result<Vec<_>>>()?;
+    let mut seen = HashSet::new();
 
-    for &expense1 in &expenses {
-        for &expense2 in &expenses {
-            if expense1 + expense2 == target {
-                println!("{} + {} = {}", expense1, expense2, expense1 + expense2);
-                println!("{} * {} = {}", expense1, expense2, expense1 * expense2);
-            }
+    for expense in util::read_lines(input)? {
+        let expense: i64 = expense?;
+
+        // The other value that would be needed to add up to target
+        let other = target - expense;
+        if seen.contains(&other) {
+            println!("{} + {} = {}", expense, other, expense + other);
+            println!("{} * {} = {}", expense, other, expense * other);
+            break;
         }
+        seen.insert(expense);
     }
 
     Ok(())
